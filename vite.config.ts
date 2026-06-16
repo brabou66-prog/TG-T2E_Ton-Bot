3818543a4e079a79e57e5dcab8f6da271107fdd9
brabou66-prog <brabou66@gmail.com>
2026-06-16 18:39:42 +0100

    Delete Vite configuration file
    Removed Vite configuration for React project.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev
-export default defineConfig({
-  base: '/TG-T2E_Ton-Bot/',
-  plugins: [react()],
-})
+// empty
